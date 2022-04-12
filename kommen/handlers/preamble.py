cda7b871895fafe877c5671130ae886b48e090db
Jason M. Pittman <jason@jasonmpittman.com>
2022-04-12 16:40:56 -0400

    updated sanitize method

--- kommen/handlers/preamble.py
+++ kommen/handlers/preamble.py
@@ -2,6 +2,7 @@
 
 """preamble.py: """
 
+import re
 import sys
 import json
 import socket
@@ -11,6 +12,7 @@ import multiprocessing
 #from handlers import firewall
 #from handlers import clients
 #from handlers import database
+from database import DatabaseHandler
 
 
 __author__ = "Jason M. Pittman"
@@ -29,21 +31,18 @@ class PreambleHandler:
     """
 
     def handle_premable(self, data):
-        plaintext = tuple(x for x in data.decode("utf-8").strip().split(','))
+        payload = tuple(x for x in data.decode("utf-8").strip().split(','))
         #client_id, count, pubkey = data.decode("utf-8").strip().split(',') # decode and split on comma to get client_id, counter
-        #decrypt payload
-        #try:
-            #decryptor = AsymmetricCryptographyHandler()
-            #plaintext = decryptor.decrypt(data, client_id)
-        #except Exception as e:
-        #    print("An exception has occured: " + str(e))
+
 
         #Server checks clients object for client_id:
         #if the client_id is in clients, server replies with a received_valid message
+        is_valid = self.is_valid_preamble(payload[0], payload[1])
+        
         #if the client_id is not in clients, server replies with a received_invalid message
         
         #return plaintext as tuple(client_id, count, key)
-        return plaintext
+        return payload
 
     def handle_exchange(self):
         pass
@@ -54,8 +53,27 @@ class PreambleHandler:
     def handle_racs(self):
         pass
 
+    def sanitize_premable(self, payload):
+        is_sanitized = False
+        client = re.match(r'/^[a-f0-9]{64}$/gi', payload[0])
+        counter = re.match(r'^[0-9]+$', int(payload[1]))
+        key = re.match(r'/^[a-f0-9]{64}$/gi', payload[0])
+        #validate client_id is size and char set expected
+        if client:
+            sanitized = True
+
+        #validate counter is integer in expected range
+        if counter:
+            sanitized = True
+
+        #validate key is size and char set expected
+        if key:
+            sanitized = True
+
+        return is_sanitized
+
     def is_valid_preamble(self, client_id, count):
-        db = database.DatabaseHandler()
+        db = DatabaseHandler()
         clients = json.dumps(db.read())
         
         if client_id in clients:
